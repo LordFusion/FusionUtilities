7f9ef3e2b1129104bcede6dd5c64b0405d3d663e
Matthew Green <LordFusion@users.noreply.github.com>
2020-02-18 01:15:33 -0600

    Restructure /poll parsing

--- src/main/java/io/github/lordfusion/fusionutilities/commands/Poll.java
+++ src/main/java/io/github/lordfusion/fusionutilities/commands/Poll.java
@@ -10,6 +10,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 // todo: /poll close - Close voting early, but still tally and execute the votes
 // todo: /poll stop - Force-stop the poll, do not pass go, do not collect votes.
 
@@ -19,7 +22,7 @@ public class Poll implements CommandExecutor
     private DataManager dataManager;
     
     private static TextComponent MSG_NO_POLL, MSG_POLL_RUNNING, MSG_POLL_YES, MSG_POLL_NO, HELP_WEATHER, HELP_TIME,
-            HELP_CUSTOM, MSG_TOO_LONG, MSG_CANNOT_AFFORD, MSG_CHRG_POLL;
+            HELP_CUSTOM, MSG_TOO_LONG, MSG_CANNOT_AFFORD, MSG_CHRG_POLL, MSG_NO_CMD;
     private static TextComponent[] ALL_HELP;
     
     public Poll()
@@ -33,6 +36,7 @@ public class Poll implements CommandExecutor
         setupMsgTooLong();
         setupMsgCannotAfford();
         setupMsgChargedForPoll();
+        setupMsgNoCmd();
         
         ALL_HELP = new TextComponent[3];
         setupMsgHelpWeather();
@@ -55,93 +59,93 @@ public class Poll implements CommandExecutor
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Console cannot participate in polls.");
-            return true;
-        }
-        
+        // Check for help
         if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
             FusionUtilities.sendUserMessages(sender, ALL_HELP);
             return true;
         }
         
-        if (instance == null || !instance.isRunning()) { // No poll running
-            if (args[0].equalsIgnoreCase("yes") || args[0].equalsIgnoreCase("no")) {
-                FusionUtilities.sendUserMessage(sender, MSG_NO_POLL);
-                return true;
-            }
-            // Make sure they can afford a poll
-            
-            
-            if (!this.dataManager.isEconomyEnabled() ||
-                    ((sender.hasPermission(FusionUtilities.PERMISSION_FREEPOLL) || this.dataManager.getEconomy()
-                            .getBalance(((Player) sender).getPlayer()) >= this.dataManager.getPollCost()))) {
-                if (args[0].equalsIgnoreCase("weather")) {
-                    // Start a weather poll?
-                    switch (args[1].toLowerCase()) {
-                        case "sunny":
-                        case "sun":
-                        case "clear":
-                            this.instance = new PollInstance(PollInstance.PollType.WEATHER, ((Player)sender).getPlayer(), "SUNNY");
-                            break;
-                        case "rain":
-                            this.instance = new PollInstance(PollInstance.PollType.WEATHER, ((Player)sender).getPlayer(), "RAIN");
-                            break;
-                        case "storm":
-                        case "thunder":
-                            this.instance = new PollInstance(PollInstance.PollType.WEATHER, ((Player)sender).getPlayer(), "STORM");
-                            break;
-                        default:
-                            FusionUtilities.sendUserMessage(sender, HELP_WEATHER);
-                            return true;
-                    }
-                } else if (args[0].equalsIgnoreCase("time")) {
-                    // Start a time poll?
-                    switch (args[1].toLowerCase()) {
-                        case "day":
-                            this.instance = new PollInstance(PollInstance.PollType.TIME, ((Player)sender).getPlayer(), "DAY");
-                            break;
-                        case "night":
-                            this.instance = new PollInstance(PollInstance.PollType.TIME, ((Player)sender).getPlayer(), "NIGHT");
-                            break;
-                        default:
-                            FusionUtilities.sendUserMessage(sender, HELP_TIME);
-                            return true;
-                    }
-                } else {
-                    // Custom poll?
-                    StringBuilder q = new StringBuilder();
-                    for (String arg : args) {
-                        q.append(arg);
-                        q.append(' ');
-                    }
-                    if (q.length() > 80) {
-                        FusionUtilities.sendUserMessage(sender, MSG_TOO_LONG);
+        if (instance == null || !instance.isRunning()) { // Poll is NOT running
+            switch (args[0]) {
+                case "yes":
+                case "no": // Attempted vote
+                    FusionUtilities.sendUserMessage(sender, MSG_NO_POLL);
+                    break;
+                case "weather":
+                case "time":
+                case "create": // Create a poll
+                    if (dataManager.chargeForPoll(sender) &&
+                            this.dataManager.getEconomy().getBalance((Player)sender) < dataManager.getPollCost()) {
+                        FusionUtilities.sendUserMessage(sender, MSG_CANNOT_AFFORD);
                         return true;
-                    } else {
-                        this.instance = new PollInstance(PollInstance.PollType.CUSTOM, ((Player)sender).getPlayer(), q.toString());
                     }
-                }
-                
-                if (this.dataManager.isEconomyEnabled() && !sender.hasPermission(FusionUtilities.PERMISSION_FREEPOLL)) {
-                    FusionUtilities.sendUserMessage(sender, MSG_CHRG_POLL);
-                }
-                
-            } else {
-                FusionUtilities.sendConsoleInfo(sender.getName() + " could not afford to start a poll.");
-                FusionUtilities.sendUserMessage(sender, MSG_CANNOT_AFFORD);
-                return true;
+                    switch (args[0]) {
+                        case "weather":
+                            switch (args[1].toLowerCase()) {
+                                case "sunny":
+                                case "sun":
+                                case "clear":
+                                    this.instance = new PollInstance(PollInstance.PollType.WEATHER, ((Player) sender).getPlayer(), "SUNNY");
+                                    break;
+                                case "rain":
+                                    this.instance = new PollInstance(PollInstance.PollType.WEATHER, ((Player) sender).getPlayer(), "RAIN");
+                                    break;
+                                case "storm":
+                                case "thunder":
+                                    this.instance = new PollInstance(PollInstance.PollType.WEATHER, ((Player) sender).getPlayer(), "STORM");
+                                    break;
+                                default:
+                                    FusionUtilities.sendUserMessage(sender, HELP_WEATHER);
+                                    break;
+                            }
+                            break;
+                        case "time":
+                            switch (args[1].toLowerCase()) {
+                                case "day":
+                                    this.instance = new PollInstance(PollInstance.PollType.TIME, ((Player) sender).getPlayer(), "DAY");
+                                    break;
+                                case "night":
+                                    this.instance = new PollInstance(PollInstance.PollType.TIME, ((Player) sender).getPlayer(), "NIGHT");
+                                    break;
+                                default:
+                                    FusionUtilities.sendUserMessage(sender, HELP_TIME);
+                                    break;
+                            }
+                            break;
+                        case "create":
+                            String q = IntStream.range(1, args.length).mapToObj(i -> args[i] + ' ').collect(Collectors.joining());
+                            if (q.length() > 80) {
+                                FusionUtilities.sendUserMessage(sender, MSG_TOO_LONG);
+                                return true;
+                            } else {
+                                this.instance = new PollInstance(PollInstance.PollType.CUSTOM, ((Player) sender).getPlayer(), q);
+                            }
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + args[0]);
+                    }
+                    break;
+                default: // Unrecognized
+                    FusionUtilities.sendUserMessage(sender, MSG_NO_CMD);
+                    break;
             }
-        } else if (args[0].equalsIgnoreCase("yes")) {
-            this.instance.voteYes(((Player) sender).getPlayer());
-            FusionUtilities.sendUserMessage(sender, MSG_POLL_YES);
-        } else if (args[0].equalsIgnoreCase("no")) {
-            this.instance.voteNo(((Player) sender).getPlayer());
-            FusionUtilities.sendUserMessage(sender, MSG_POLL_NO);
-        } else {
-            FusionUtilities.sendUserMessage(sender, MSG_POLL_RUNNING);
+        } else { // Poll IS running
+            switch (args[0]) {
+                case "yes":
+                    this.instance.voteYes(((Player) sender).getPlayer());
+                    FusionUtilities.sendUserMessage(sender, MSG_POLL_YES);
+                    break;
+                case "no":
+                    this.instance.voteNo(((Player) sender).getPlayer());
+                    FusionUtilities.sendUserMessage(sender, MSG_POLL_NO);
+                    break;
+                case "weather":
+                case "time":
+                case "default":
+                    FusionUtilities.sendUserMessage(sender, MSG_POLL_RUNNING);
+                    break;
+            }
         }
-        
         return true;
     }
     
@@ -181,7 +185,12 @@ public class Poll implements CommandExecutor
     private void setupMsgChargedForPoll()
     {
         MSG_CHRG_POLL = new TextComponent("You were charged to start this poll: $");
-        MSG_CANNOT_AFFORD.setColor(ChatColor.YELLOW);
+        MSG_CHRG_POLL.setColor(ChatColor.YELLOW);
+    }
+    private void setupMsgNoCmd()
+    {
+        MSG_NO_CMD = new TextComponent("Unrecognized command. Try /poll help");
+        MSG_NO_CMD.setColor(ChatColor.RED);
     }
     private void setupMsgHelpWeather()
     {
@@ -197,7 +206,7 @@ public class Poll implements CommandExecutor
     }
     private void setupMsgHelpCustom()
     {
-        HELP_CUSTOM = new TextComponent("/poll <QUESTION>");
+        HELP_CUSTOM = new TextComponent("/poll create <QUESTION>");
         HELP_CUSTOM.setColor(ChatColor.DARK_AQUA);
         ALL_HELP[2] = HELP_CUSTOM;
     }
