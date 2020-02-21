dc4bd2cb32f7ca13b45c5341bd520cdecd49073b
Matthew Green <LordFusion@users.noreply.github.com>
2020-02-20 21:19:34 -0600

    Fix bug in console messaging Fixes a bug where sending a TextComponent message with extras to the Console would only print the base TextComponent, and none of the extras.

--- src/main/java/io/github/lordfusion/fusionutilities/commands/Kickout.java
+++ src/main/java/io/github/lordfusion/fusionutilities/commands/Kickout.java
@@ -1,11 +1,27 @@
 package io.github.lordfusion.fusionutilities.commands;
 
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.Town;
+import io.github.lordfusion.fusionutilities.DataManager;
+import io.github.lordfusion.fusionutilities.FusionUtilities;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Kickout implements CommandExecutor
 {
+    private DataManager dataManager;
+    
+    private static TextComponent HELP;
+    private static TextComponent MSG_CONSOLE_UNAVAILABLE, MSG_TOWNY_DISABLED, MSG_NOT_RESIDENT, MSG_NO_TOWN;
+    
+    public Kickout()
+    {
+        this.dataManager = FusionUtilities.getInstance().getDataManager();
+    }
     
     /**
      * Executes the given command, returning its success.
@@ -22,6 +38,41 @@ public class Kickout implements CommandExecutor
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
+        // Ensure that the command is sent by a player
+        if (!(sender instanceof Player)) {
+            FusionUtilities.sendUserMessage(sender, MSG_CONSOLE_UNAVAILABLE);
+            return true;
+        }
+        // Ensure that Towny is available
+        if (!this.dataManager.isTownyEnabled()) {
+            FusionUtilities.sendUserMessage(sender, MSG_TOWNY_DISABLED);
+            FusionUtilities.sendConsoleWarn("Tried to use /kickout while Towny is missing!");
+            return true;
+        }
+        // Ensure that there is an argument
+        if (args.length != 1) {
+            FusionUtilities.sendUserMessage(sender, HELP);
+            FusionUtilities.sendConsoleInfo(sender.getName() + " could not use /kickout; Incorrect arguments.");
+            return true;
+        }
+    
+        Resident resident = this.dataManager.getTowny().getTownyUniverse().getResidentMap().get(sender.getName());
+        if (resident == null) {
+            FusionUtilities.sendUserMessage(sender, MSG_NOT_RESIDENT);
+            FusionUtilities.sendConsoleInfo(sender.getName() + " could not use /kickout; They are not a Resident.");
+            return true;
+        }
+        Town town;
+        try {
+            town = resident.getTown();
+        } catch (NotRegisteredException e) {
+            FusionUtilities.sendUserMessage(sender, MSG_NO_TOWN);
+            FusionUtilities.sendConsoleInfo(sender.getName() + " could not use /kickout; They are not in a town.");
+            return true;
+        }
+        
+        
+        
         return true;
     }
 }
